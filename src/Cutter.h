76e28689b389f0e4756499f87bf71baae3af3296
fcasal <fcasal@users.noreply.github.com>
2018-07-30 07:55:39 +0100

    Adds step out funcionality (#598)
    * Added step out functionality to toolbar and its shortcut
    * Remove step out button when emulating

--- src/Cutter.h
+++ src/Cutter.h
@@ -512,6 +512,7 @@ public:
     void continueUntilDebug(QString offset);
     void stepDebug();
     void stepOverDebug();
+    void stepOutDebug();
     void toggleBreakpoint(RVA addr);
     void toggleBreakpoint(QString addr);
     void delBreakpoint(RVA addr);
